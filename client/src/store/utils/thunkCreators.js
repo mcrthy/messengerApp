b0147cc6ca5d3e661ca2a068533602508cac6e4b
Matt McCarthy <matt_22_mccarthy@live.com>
2021-06-18 20:30:44 -0400

    removed unnecessary conversationID value

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -91,13 +91,14 @@ const sendMessage = (data, body) => {
   });
 };
 
-// message format to send: {recipientId, text, conversationId}
-// conversationId will be set to null if its a brand new conversation
+// message format to send: {recipientId, text, sender}
+// sender will be set to null if a conversation already exists
 export const postMessage = (body) => async (dispatch) => {
   try {
+
     const data = await saveMessage(body);
 
-    if (!body.conversationId) {
+    if (body.sender) {
       dispatch(addConversation(body.recipientId, data.message));
     } else {
       dispatch(setNewMessage(data.message));
